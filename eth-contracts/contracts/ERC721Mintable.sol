7c01a3a856d695f6a79fbe8e46e0fdb245bddbfa
alice <bow1226@gmail.com>
2022-03-28 15:25:01 +0800

    feat: Implement Internal function to mint a new token

--- eth-contracts/contracts/ERC721Mintable.sol
+++ eth-contracts/contracts/ERC721Mintable.sol
@@ -276,9 +276,13 @@ contract ERC721 is Pausable, ERC165 {
     // @dev Internal function to mint a new token
     // TIP: remember the functions to use for Counters. you can refresh yourself with the link above
     function _mint(address to, uint256 tokenId) internal {
-        // TODO revert if given tokenId already exists or given address is invalid
-        // TODO mint tokenId to given address & increase token count of owner
-        // TODO emit Transfer event
+        // revert if given tokenId already exists or given address is invalid
+        require(!_exists(tokenId) && to != address(0));
+        // mint tokenId to given address & increase token count of owner
+        _tokenOwner[tokenId] = to;
+        _ownedTokensCount[to].increment();
+        // emit Transfer event
+        emit Transfer(address(0), to, tokenId);
     }
 
     // @dev Internal function to transfer ownership of a given token ID to another address.
