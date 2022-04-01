c638aa373ac823804e502ffec04ce37cf1fc62cc
alice <bow1226@gmail.com>
2022-04-01 19:17:26 +0800

    feat: Implement a function to mint new NFT only after the solution has been verified

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -31,18 +31,27 @@ contract SolnSquareVerifier is CustomERC721Token {
         emit AddSolution(to, tokenId);
     }
 
+    function verifyTx(
+        uint256[2] memory a,
+        uint256[2][2] memory b,
+        uint256[2] memory c,
+        uint256[2] memory input
+    ) public returns (bool r) {}
+
     // Create a function to mint new NFT only after the solution has been verified
     //  - make sure the solution is unique (has not been used before)
     //  - make sure you handle metadata as well as tokenSuplly
     function mintNewNFT(
         uint256 a,
         uint256 b,
+        uint256 c,
+        uint256 input,
         address to,
         uint256 tokenId
     ) public {
-        bytes32 key = keccak256(abi.encodePacked(a, b));
+        bytes32 key = keccak256(abi.encodePacked(a, b, c, input));
         require(uniqueSolutions[key].to == address(0));
-
+        require(verifierContract.verifyTx(a, b, c, input));
         addSolution(to, tokenId, key);
         super._mint(to, tokenId);
     }
