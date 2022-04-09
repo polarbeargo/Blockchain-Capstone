e43b841700e01bf6ade890da88f6082401066b59
Hsin-Wen Chang <bow1226@gmail.com>
2022-04-09 13:55:57 +0800

    test:  if an ERC721 token can be minted for contract and a new solution can be added for contract

--- eth-contracts/test/TestSolnSquareVerifier.js
+++ eth-contracts/test/TestSolnSquareVerifier.js
@@ -1,3 +1,25 @@
 // Test if a new solution can be added for contract - SolnSquareVerifier
 
 // Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+let SquareVerifier = artifacts.require('BN256G2.sol');
+const SolnSquareVerifier = artifacts.require('SolnSquareVerifier');
+
+contract('Test SolnSquareVerifier', accounts => {
+
+    describe('test verifier with proof', function () {
+        beforeEach(async function () {
+            this.contract = await SquareVerifier.new({from: accounts[0]});
+        })
+
+        it('verify with correct proof', async function () { 
+            let verification = await this.contract.verifyTx.call();
+            assert.equal(verification, true, 'Verification is invalid');
+        })
+
+        it('verify failure with incorrect proof', async function () { 
+            let verification = await this.contract.verifyTx.call();
+            assert.equal(verification, false, 'Verification is valid');
+        })
+
+    })
+});
