dda23ee5f2c10bdee9bff598aa6f29993447ee38
Hsin-Wen Chang <bow1226@gmail.com>
2022-04-10 13:58:19 +0800

    test: Implement TestSolnSquareVerifier
    - Test if a new solution can be added for contract
    - SolnSquareVerifier
    Test if an ERC721 token can be minted for contract

--- eth-contracts/test/TestSolnSquareVerifier.js
+++ eth-contracts/test/TestSolnSquareVerifier.js
@@ -1,25 +1,39 @@
 // Test if a new solution can be added for contract - SolnSquareVerifier
-
 // Test if an ERC721 token can be minted for contract - SolnSquareVerifier
-let SquareVerifier = artifacts.require('BN256G2.sol');
-const SolnSquareVerifier = artifacts.require('SolnSquareVerifier');
+var SolnSquareVerifier = artifacts.require('SolnSquareVerifier');
+var Verifier = artifacts.require('BN256G2');
 
 contract('Test SolnSquareVerifier', accounts => {
 
-    describe('test verifier with proof', function () {
+    describe('test solnSquareVerifier with proof', function () {
         beforeEach(async function () {
-            this.contract = await SquareVerifier.new({from: accounts[0]});
+            const _verifier = await Verifier.new({from: accounts[0]});
+            this.contract = await SolnSquareVerifier.new(_verifier.address, {from: accounts[0]});
         })
 
-        it('verify with correct proof', async function () { 
-            let verification = await this.contract.verifyTx.call();
-            assert.equal(verification, true, 'Verification is invalid');
+        it('if a new solution can be added for contract and token minted - SolnSquareVerifier', async function () { 
+            let success = true;
+            try {
+                await this.contract.mintNewNFT();
+            } catch(error) {
+                success = false;
+            }
+
+            assert.equal(success, true, 'Can not add solution')
         })
 
-        it('verify failure with incorrect proof', async function () { 
-            let verification = await this.contract.verifyTx.call();
-            assert.equal(verification, false, 'Verification is valid');
-        })
+        it('if a repeat solution can be added for contract - SolnSquareVerifier', async function () { 
+            let success = true;
 
+            await this.contract.mintNewNFT();
+
+            try {
+                await this.contract.mintNewNFT();
+            } catch(error) {
+                success = false;
+            }
+
+            assert.equal(success, false, 'Added repeat solution')
+        })
     })
-});
+})
