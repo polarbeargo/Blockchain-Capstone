54314b82819774a50bcb0dfc8d0d01366827bdb8
Hsin-Wen Chang <bow1226@gmail.com>
2022-05-13 16:24:02 +0800

    style: Clean code

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -37,13 +37,6 @@ contract SolnSquareVerifier is CustomERC721Token {
         emit AddSolution(to, tokenId);
     }
 
-    function verifyTx(
-        uint256[2] memory a,
-        uint256[2][2] memory b,
-        uint256[2] memory c,
-        uint256[2] memory input
-    ) public returns (bool r) {}
-
     // Create a function to mint new NFT only after the solution has been verified
     //  - make sure the solution is unique (has not been used before)
     //  - make sure you handle metadata as well as tokenSuplly
